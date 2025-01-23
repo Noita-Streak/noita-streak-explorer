4d5534850a713b782f27b6db90109416223fc110
den3606 <den3606.dev@gmail.com>
2025-01-24 04:48:21 +0900

    refactor: format

--- files/scripts/rewrite/perks/glass_cannon_enemy.lua
+++ files/scripts/rewrite/perks/glass_cannon_enemy.lua
@@ -9,8 +9,10 @@ local function rewrite_code(content)
   -- config_explosionのダメージ修正
   content = content:gsub("v = v %* 4%.0", "if (v ~= nil) then v = v * 0.2 * 4.0")
   -- 存在しない項目読み込みによるnilエラー回避
-  content = content:gsub("ComponentObjectSetValue%( comp, %\"config_explosion%\", b, tostring%(v%) %)",
-    "ComponentObjectSetValue( comp, \"config_explosion\", b, tostring(v) ) end")
+  content = content:gsub(
+    'ComponentObjectSetValue%( comp, %"config_explosion%", b, tostring%(v%) %)',
+    'ComponentObjectSetValue( comp, "config_explosion", b, tostring(v) ) end'
+  )
   return content
 end
 
@@ -35,5 +37,5 @@ end
 
 return {
   rewrite = rewrite,
-  rewrite_code = rewrite_code
+  rewrite_code = rewrite_code,
 }
