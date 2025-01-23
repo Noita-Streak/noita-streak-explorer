dfc9f35373638f41958ab8f36bce89abb2ed4c85
den3606 <den3606.dev@gmail.com>
2025-01-23 23:28:34 +0900

    feat: MD5チェックの追加、書き換えの関数化

--- files/scripts/rewrite/entry.lua
+++ files/scripts/rewrite/entry.lua
@@ -1,1 +1,2 @@
-dofile_once("mods/noita-streak-explorer/files/scripts/rewrite/perks/glass_cannon_enemy.lua")
+local file = dofile_once("mods/noita-streak-explorer/files/scripts/rewrite/perks/glass_cannon_enemy.lua")
+file.rewrite()
