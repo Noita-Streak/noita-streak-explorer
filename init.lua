c2922b135fb11e2a2cb81acfd219fa887ad9cd06
den3606 <den3606.dev@gmail.com>
2025-01-25 20:56:59 +0900

    feat: ガラスキャノンの修正を設定で変更できるようにした

--- init.lua
+++ init.lua
@@ -1,32 +1,29 @@
-dofile_once("mods/noita-streak-explorer/files/scripts/rewrite/entry.lua")
+dofile_once("mods/noita-streak-explorer/files/scripts/rewrite/entry.lua").rewrite()
 
--- function OnModPreInit()
--- 	print("Mod - OnModPreInit()")
--- end
+function OnModPreInit()
+  print("Mod - OnModPreInit()")
+end
 
--- function OnModInit()
--- 	print("Mod - OnModInit()")
--- end
+function OnModInit()
+  print("Mod - OnModInit()")
+end
 
--- function OnModPostInit()
--- 	print("Mod - OnModPostInit()")
--- end
+function OnModPostInit()
+  print("Mod - OnModPostInit()")
+end
 
--- function OnPlayerSpawned( player_entity )
--- 	GamePrint( "OnPlayerSpawned() - Player entity id: " .. tostring(player_entity) )
--- end
+function OnPlayerSpawned(_player_entity) end
 
--- function OnWorldInitialized()
--- 	GamePrint( "OnWorldInitialized() " .. tostring(GameGetFrameNum()) )
--- end
+function OnWorldInitialized()
+  -- GamePrint("OnWorldInitialized() " .. tostring(GameGetFrameNum()))
+end
 
--- function OnWorldPreUpdate()
--- 	GamePrint( "Pre-update hook " .. tostring(GameGetFrameNum()) )
--- end
+function OnWorldPreUpdate()
+  -- GamePrint("Pre-update hook " .. tostring(GameGetFrameNum()))
+end
 
--- function OnWorldPostUpdate()
--- 	GamePrint( "Post-update hook " .. tostring(GameGetFrameNum()) )
--- end
+function OnWorldPostUpdate()
+  -- GamePrint("Post-update hook " .. tostring(GameGetFrameNum()))
+end
 
--- function OnMagicNumbersAndWorldSeedInitialized()
--- end
+function OnMagicNumbersAndWorldSeedInitialized() end
