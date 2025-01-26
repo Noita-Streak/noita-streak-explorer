3b08ee05e15b69c8d9559f11202d5c8e0f8df285
den3606 <den3606.dev@gmail.com>
2025-01-26 19:01:03 +0900

    feat: ジャングルのドラゴンスポーン処理を消した

--- files/scripts/const.lua
+++ files/scripts/const.lua
@@ -3,5 +3,6 @@ local mod_name = "noita-streak-explorer"
 return {
   MOD_SETTINGS = {
     FIX_GLASS_CANNON_ENEMY = mod_name .. ".option.fix_glass_cannon_enemy",
+    DISABLE_DRAGON_IN_JUNGLE = mod_name .. ".option.disable_dragon_in_jungle",
   },
 }
